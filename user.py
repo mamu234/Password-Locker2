f5a7dde8117202e527e66b6058453b0ef8496765
carolyne <pcmaunda@gmail.com>
2022-04-25 08:19:43 +0300

    created function for saving user

--- user.py
+++ user.py
@@ -4,13 +4,13 @@ class User :
    """
 
    userlist = []
-   def __init__(self,first_name,last_name,password,email, phone_number):
+   def __init__(self,first_name,last_name,phone_number,password,email, ):
        self.first_name = first_name
        self.last_name = last_name
+       self.phone_number = phone_number
        self.password = password
        self.email = email
-       self.phone_number = phone_number
-
+       
    def  save_user(self):
 
         """
--- user_test.py
+++ user_test.py
@@ -18,9 +18,16 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,"carolyne")
         self.assertEqual(self.new_user.last_name, "Maunda")
         self.assertEqual(self.new_user.phone_number,"0721830476")
-        self.assertEqual(self.new_user.email,"pcmaunda@gmail.com")
         self.assertEqual(self.new_user.password,"1234")
-
+        self.assertEqual(self.new_user.email,"pcmaunda@gmail.com")
+    
+    def test_save_user(self):
+        '''
+        test_save_uer test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.userlist),1)
 
 if __name__ == '__main__':
     unittest.main()
