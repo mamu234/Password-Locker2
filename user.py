920f299716e538f64c051831f99e4816bb287b80
carolyne <pcmaunda@gmail.com>
2022-04-26 18:44:43 +0300

    updated user class

--- user.py
+++ user.py
@@ -1,103 +1,34 @@
-import pyperclip
 
 
-class User :
-   """
-   Class that generates new instance of user
-   """
+class User:
 
-   userlist = []
-   def __init__(self,first_name,last_name,phone_number,password,email, ):
-       self.first_name = first_name
-       self.last_name = last_name
-       self.phone_number = phone_number
-       self.password = password
-       self.email = email
-       
-
-   def  save_user(self):
-
-        """
-        save-user method is used to  create new user objects to the user_list 
-        """     
-        User.userlist.append(self)
+    userlist = []
+    def __init__(self, first_name, last_name, phone_number, email,password):
+      self.first_name = first_name
+      self.last_name = last_name
+      self.phone_number = phone_number
+      self.email = email
+      self.password = password
     
-   
-   def delete_user(self):# delete method deletes saved user from the userlist
+
+    def save_user(self):
+
+      self.userlist.append(self)
     
+    @classmethod
+    def delete_user(cls):
+        for user in cls.userlist:
+            if user == user:
+                cls.userlist.remove(user)
+                return True
+            else: return False
+
+
  
-         
-       User.userlist.remove(self)
-
-
-
-   @classmethod
-   def display_users(cls):
-       return cls.userlist
-
+    
    
-   @classmethod
-   def copy_email(cls,number):
-    contact_found = User.find_by_number(number)
-    pyperclip.copy(contact_found.email)
-
-   @classmethod
-   def find_by_number(cls,number):
-
-       for user in cls.userlist:
-           if user.phone_number == number:
-               return user
-
              
 
-       else:
-            for user in cls.userlist:
-                cls.userlist.remove(user)
-
-class Credentials:
-    """
-    this class generates new instance of Credentials
-    """
-
-    credentials_list =[]
-    def __init__(self,account_username,account_name,account_password,email):
-        """
-        init methos helps define properties of the object
-        """
-
-        self.account_username = account_username
-        self.account_name = account_name
-        self.account_password = account_password
-        self.email = email
-        
-    def save_credentials(self):
-        """
-        save account method saves user info in the accounts
-        """
-
-        Credentials.credentials_list.append(self)
-    
-
-    def delete_credentials(self):
-        """
-        delete_account  method removes the saved credentials from accounts
-
-        """
-    def copy_email(self):
-        '''
-        Test to confirm that we are copying the email address from a found user
-        '''
-    def display_all_credentials(self):
-        '''
-        method that returns a list of all accounts saved
-        '''
-    def save_multiple_credentials(self):
-    
-    
-        
-
-
-        Credentials.credentials_list.remove(self)
     
 
 
--- user_test.py
+++ user_test.py
@@ -1,13 +1,13 @@
 import unittest 
-from user import Credentials, User
-import pyperclip
+from user import  User
+# import pyperclip
 
 class TestUser(unittest.TestCase):
     def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user= User("carolyne","Maunda","0721830476","1234", "pcmaunda@gmail.com") # create user object
+        self.new_user= User("carolyne","Maunda","0721830476","pcmaunda@gmail.com","1234") # create user object
 
 
     def test_init(self):
@@ -18,12 +18,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,"carolyne")
         self.assertEqual(self.new_user.last_name, "Maunda")
         self.assertEqual(self.new_user.phone_number,"0721830476")
-        self.assertEqual(self.new_user.password,"1234")
         self.assertEqual(self.new_user.email,"pcmaunda@gmail.com")
+        self.assertEqual(self.new_user.password,"1234")
+   
     
     def test_save_user(self):
         '''
-        test_save_uer test case to test if the user object is saved into
+        test_save_user test case to test if the user object is saved into
          the user list
         '''
         self.new_user.save_user() # saving the new user
@@ -57,144 +58,144 @@ class TestUser(unittest.TestCase):
             self.new_user.delete_user()# Deleting a user object
             self.assertEqual(len(User.userlist),1)
 
-    def test_find_user_by_number(self):
-        '''
-        test to check if we can find a contact by phone number and display information
-        '''
+#     def test_find_user_by_number(self):
+#         '''
+#         test to check if we can find a contact by phone number and display information
+#         '''
 
-        self.new_user.save_user()
-        test_user = User("Test","user","0721830476","pcmaunda@gmail.com","1234") # new user
-        test_user.save_user()
+#         self.new_user.save_user()
+#         test_user = User("Test","user","0721830476","pcmaunda@gmail.com","1234") # new user
+#         test_user.save_user()
 
-        found_user = User.find_by_number("0721830476")
+#         found_user = User.find_by_number("0721830476")
 
-        self.assertEqual(found_user,test_user.phone_number)
+#         self.assertEqual(found_user,test_user.phone_number)
 
 
-    def test_user_exists(self):
-        '''
-        test to check if we can return a Boolean  if we cannot find the cuser.
-        '''
+#     def test_user_exists(self):
+#         '''
+#         test to check if we can return a Boolean  if we cannot find the cuser.
+#         '''
 
-        self.new_user.save_user()
-        test_user = User("Test","user","0721830476","pcmaunda@gmail.com","1234")
-        test_user.save_user()
-        user_exists = User.save_user("0721830476")
-        self.assertTrue(user_exists)
+#         self.new_user.save_user()
+#         test_user = User("Test","user","0721830476","pcmaunda@gmail.com","1234")
+#         test_user.save_user()
+#         user_exists = User.save_user("0721830476")
+#         self.assertTrue(user_exists)
     
     
-    def test_display_all_users(self):
-        '''
-        method that returns a list of all users saved
-        '''
+#     def test_display_all_users(self):
+#         '''
+#         method that returns a list of all users saved
+#         '''
 
-        self.assertEqual(User.display_users(),User.userlist)
+#         self.assertEqual(User.display_users(),User.userlist)
 
-    def copy_email(self):
-        '''
-        Test to confirm that we are copying the email address from a found user
-        '''
+#     def copy_email(self):
+#         '''
+#         Test to confirm that we are copying the email address from a found user
+#         '''
 
-        self.new_user.save_user()
-        User.copy_email("0721830476")
+#         self.new_user.save_user()
+#         User.copy_email("0721830476")
 
-        self.assertEqual(self.new_user.email,pyperclip.paste())
+#         self.assertEqual(self.new_user.email,pyperclip.paste())
     
-    @classmethod
-    def copy_email(cls,number):
-        contact_found = User.find_by_number(number)
-        pyperclip.copy(contact_found.email)
+#     @classmethod
+#     def copy_email(cls,number):
+#         contact_found = User.find_by_number(number)
+#         pyperclip.copy(contact_found.email)
 
 
-class TestCredentials(unittest.TestCase):
-    def setUp(self):
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_credential= Credentials("carolyne","Maunda","12345","pcmaunda@gmail.com") # create credential object
+# class TestCredentials(unittest.TestCase):
+#     def setUp(self):
+#         '''
+#         Set up method to run before each test cases.
+#         '''
+#         self.new_credential= Credentials("carolyne","Maunda","12345","pcmaunda@gmail.com") # create credential object
 
 
-    def test_init(self):
-        '''
-        test_init test case to test if the object is initialized properly
-        '''
+#     def test_init(self):
+#         '''
+#         test_init test case to test if the object is initialized properly
+#         '''
 
-        self.assertEqual(self.new_credential.account_username,"carolyne")
-        self.assertEqual(self.new_credential.account_name, "Maunda")
-        self.assertEqual(self.new_credential.account_password,"12345")
-        self.assertEqual(self.new_credential.email,"pcmaunda@gmail.com")
+#         self.assertEqual(self.new_credential.account_username,"carolyne")
+#         self.assertEqual(self.new_credential.account_name, "Maunda")
+#         self.assertEqual(self.new_credential.account_password,"12345")
+#         self.assertEqual(self.new_credential.email,"pcmaunda@gmail.com")
        
     
-    def test_save_credentials(self):
-        '''
-        test_save_credential  test case to test if the credential object is saved into
-         the  accountlist
-        '''
-        self.new_credential.save_credentials() 
-        self.assertEqual(len(Credentials.save_credentials),1)
+#     def test_save_credentials(self):
+#         '''
+#         test_save_credential  test case to test if the credential object is saved into
+#          the  accountlist
+#         '''
+#         self.new_credential.save_credentials() 
+#         self.assertEqual(len(Credentials.save_credentials),1)
 
 
-    def tearDown(self):
-            '''
-            tearDown method that does clean up after each test case has  run.
-            '''
-            Credentials.credentials_list = []
+#     def tearDown(self):
+#             '''
+#             tearDown method that does clean up after each test case has  run.
+#             '''
+#             Credentials.credentials_list = []
 
-    def test_save_multiple_credentials(self):
-            '''
-            test_save_multiple_credentials to check if we can save multiple crdentials
-            objects to our user_accounts
-            '''
-            self.new_credential.credentials_list()
-            test_credential = Credentials("Test","credential","carolyne","maunda")
-            test_credential.credentials_list()
-            self.assertEqual(len(Credentials.credentials_list),2)
+#     def test_save_multiple_credentials(self):
+#             '''
+#             test_save_multiple_credentials to check if we can save multiple crdentials
+#             objects to our user_accounts
+#             '''
+#             self.new_credential.credentials_list()
+#             test_credential = Credentials("Test","credential","carolyne","maunda")
+#             test_credential.credentials_list()
+#             self.assertEqual(len(Credentials.credentials_list),2)
         
-    def test_delete_credential(self):
-            '''
-            test_delete_credential tests if we can remove a  credential  from our user_accounts
-            '''
-            self.new_credential.credentials_list()
-            test_credential = Credentials("Test","credential","carolyne","maunda")
-            test_credential.credentials_list()
+#     def test_delete_credential(self):
+#             '''
+#             test_delete_credential tests if we can remove a  credential  from our user_accounts
+#             '''
+#             self.new_credential.credentials_list()
+#             test_credential = Credentials("Test","credential","carolyne","maunda")
+#             test_credential.credentials_list()
 
-            self.new_credential.delete_credentials()# Deleting a user object
-            self.assertEqual(len(Credentials.credentials_list),1)
+#             self.new_credential.delete_credentials()# Deleting a user object
+#             self.assertEqual(len(Credentials.credentials_list),1)
 
    
-    def test_credential_exists(self):
-        '''
-        test to check if we can return a Boolean  if we cannot find the account.
-        '''
+#     def test_credential_exists(self):
+#         '''
+#         test to check if we can return a Boolean  if we cannot find the account.
+#         '''
 
-        self.new_credential.credentials_list()
-        test_credential = Credentials("Test","credential","carolyne","maunda")
-        test_credential.credentials_list()
+#         self.new_credential.credentials_list()
+#         test_credential = Credentials("Test","credential","carolyne","maunda")
+#         test_credential.credentials_list()
 
-        credential_exists = Credentials.credentials_list("carolyne")
+#         credential_exists = Credentials.credentials_list("carolyne")
 
-        self.assertTrue(credential_exists)
+#         self.assertTrue(credential_exists)
     
     
-    def test_display_all_credentials(self):
-        '''
-        method that returns a list of all accounts saved
-        '''
+#     def test_display_all_credentials(self):
+#         '''
+#         method that returns a list of all accounts saved
+#         '''
 
-        self.assertEqual(Credentials.display_all_credentials(),Credentials.credentials_list)
+#         self.assertEqual(Credentials.display_all_credentials(),Credentials.credentials_list)
 
-    def test_copy_email(self):
-        '''
-        Test to confirm that we are copying the email address from a found account
-        '''
+#     def test_copy_email(self):
+#         '''
+#         Test to confirm that we are copying the email address from a found account
+#         '''
 
 
-        self.assertEqual(self.new_credential.email,pyperclip.paste())
+#         self.assertEqual(self.new_credential.email,pyperclip.paste())
     
-    @classmethod
-    def copy_email(cls,number):
-        credential_found = Credentials.find_by_number(number)
-        pyperclip.copy(credential_found.email)
+#     @classmethod
+#     def copy_email(cls,number):
+#         credential_found = Credentials.find_by_number(number)
+#         pyperclip.copy(credential_found.email)
 
 
 
